46e98027d375e730096682c88a880609b3c03216
卢本伟 <lubenwei@163.com>
2020-09-26 01:52:12 +0800

    增删改

--- bookmanager/book/views.py
+++ bookmanager/book/views.py
@@ -2,7 +2,25 @@ from django.shortcuts import render
 
 # Create your views here.
 from django.http import  HttpResponse
-
+from book.models import BookInfo
 def index(request):
-   return HttpResponse('ok!')
+   books=BookInfo.objects.all()
+   print(books)
+
+   return HttpResponse('index')
+
+###############增加############
+from book.models import BookInfo
+book = BookInfo(
+   name = 'Django',
+   pub_date='2000-1-1',
+   readcount=10
+)
+book.save()
+
+BookInfo.objects.create(
+   name='测试开发入门',
+   pub_date='2020-1-1',
+   readcount=100
+)
 
