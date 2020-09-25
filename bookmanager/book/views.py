52ed5d932df7c196ff3e2ac4064e58ea89e79bb8
卢本伟 <lubenwei@163.com>
2020-09-25 23:48:46 +0800

    模板

--- bookmanager/book/views.py
+++ bookmanager/book/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.http import  HttpResponse
+
+def index(request):
+    context = {'title':'测试模板数据'}
+    return render(request,'book/index.html',context)
