8af2b388b4bc26e53295909c1aff8680d31c4358
卢本伟 <lubenwei@163.com>
2020-09-25 22:45:55 +0800

    新站点管理

--- bookmanager/book/admin.py
+++ bookmanager/book/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
-
+from book.models import BookInfo,PeopleInfo
+# from book
 # Register your models here.
+admin.site.register(BookInfo)
+admin.site.register(PeopleInfo)
