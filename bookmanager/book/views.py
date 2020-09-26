77a2fd0a40c270765b52f9e312044a0a0a94a3a4
卢本伟 <lubenwei@163.com>
2020-09-26 16:01:39 +0800

    FQ

--- bookmanager/book/views.py
+++ bookmanager/book/views.py
@@ -15,25 +15,3 @@ book = BookInfo(
    readcount=10
 )
 book.save()
-
-BookInfo.objects.create(
-   name='测试开发入门',
-   pub_date='2020-1-1',
-   readcount=100
-)
-from book.models import BookInfo
-try:
-   book=BookInfo.objects.get(id=3)
-
-except BookInfo.DoesNotExist:
-   print('查询结果不存在')
-
-BookInfo.objects.all()
-from book.models import PeopleInfo
-PeopleInfo.objects.all()
-
-BookInfo.objects.all().count()
-BookInfo.objects.count()
-
-from django.db.models import F
-BookInfo.objects.filter(readcount__gt=F('commentcount'))
